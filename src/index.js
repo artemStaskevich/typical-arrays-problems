03043d87762af8f2ba64119a135017a2b68beeb5
Artem <artem-git@clinicbuddy.com>
2020-02-28 11:37:46 +0300

    feat: add solved Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined ||  array == null || array.length == 0) {
+    return 0;
+  }
+
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined ||  array == null || array.length == 0) {
+    return 0;
+  }
+
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined ||  array == null || array.length == 0) {
+    return 0;
+  }
+
+  var summ = array.reduce(function(sum, current) {
+    return sum + current;
+  }, 0);
+
+  return summ/array.length;
 }
